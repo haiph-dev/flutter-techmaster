e5fe90b2c67490771a18f30fcb36491afda6c673
haiph-dev <pinkocean.a16@gmail.com>
2023-06-09 22:10:13 +0700

    update

--- lib/listview/listViewDemo.dart
+++ lib/listview/listViewDemo.dart
@@ -1,6 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'user/user.dart';
 
+//fetch users from API
+Future<List<User>> fetchUsers(http.Client client) async {
+  final response =
+      await client.get(Uri.parse('http://api.quynhtao.com/api/users'));
+  print(response.body);
+  return compute(parseUsers, response.body);
+}
+
+List<User> parseUsers(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<User>((json) => User.fromJson(json)).toList();
+}
+
+//build page
 class ListViewDemo extends StatefulWidget {
   const ListViewDemo({super.key});
 
@@ -11,17 +29,47 @@ class ListViewDemo extends StatefulWidget {
 class _ListViewDemoState extends State<ListViewDemo> {
   @override
   Widget build(BuildContext context) {
-    Future<http.Response> fetchUser(http.Client client) async {
-      return client.get(Uri.parse('http://api.quynhtao.com/api/users'));
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('List View Demo'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('List View Demo'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: FutureBuilder<List<User>>(
+            future: fetchUsers(http.Client()),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text('Network Error', style: TextStyle(fontSize: 50)),
+                );
+              } else if (snapshot.hasData) {
+                return UserListView(users: snapshot.data!);
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }));
+  }
+}
+
+class UserListView extends StatelessWidget {
+  final List<User> users;
+
+  const UserListView({super.key, required this.users});
+
+  @override
+  Widget build(BuildContext context) {
+    print(users);
+    return ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(users[index].avatar!),
+              )
+            ],
+          );
+        });
   }
 }
