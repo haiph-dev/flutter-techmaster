59961aa22ddb9cb2584c226f5d2cb8c1ac4d30ad
haiph-dev <pinkocean.a16@gmail.com>
2023-06-12 22:48:24 +0700

    update

--- lib/cardGame/cardPage.dart
+++ lib/cardGame/cardPage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +16,44 @@ class CardGame extends StatelessWidget {
   }
 }
 
+class SingletonCardList {
+  List<CardItem> list = [];
+  final String name = 'SingletonCardList';
+
+  static final SingletonCardList _singleton = SingletonCardList._internal();
+
+  factory SingletonCardList() {
+    return _singleton;
+  }
+
+  SingletonCardList._internal();
+
+  void initList({required List<CardItem> input}) {
+    list = input;
+    list.shuffle();
+    for (int i = 0; i < list.length; i++) {
+      list[i].index = i;
+    }
+  }
+
+  List<CardItem> getList() {
+    return list;
+  }
+
+  void checkMatch(int index) {
+    for (int i = 0; i < list.length; i++) {
+      if (list[i].isOpen) {
+        if (i != index && list[i].type == list[index].type) {
+          list[i].isMatch = true;
+          list[index].isMatch = true;
+        } else {
+          list[i].isOpen = false;
+        }
+      }
+    }
+  }
+}
+
 class CardGameView extends StatefulWidget {
   const CardGameView({super.key});
 
@@ -22,7 +62,14 @@ class CardGameView extends StatefulWidget {
 }
 
 class _CardGameViewState extends State<CardGameView> {
-  List<CardItem> cardList = List.generate(6, (index) => CardItem(type: 'cat'));
+  final singletonCardList = SingletonCardList().initList(input: [
+    CardItem(type: 'cat'),
+    CardItem(type: 'cat'),
+    CardItem(type: 'dog'),
+    CardItem(type: 'dog'),
+    CardItem(type: 'pig'),
+    CardItem(type: 'pig')
+  ]);
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +77,7 @@ class _CardGameViewState extends State<CardGameView> {
       padding: const EdgeInsets.all(10),
       child: GridView.count(
         crossAxisCount: 2,
-        children: cardList,
+        children: SingletonCardList().list,
       ),
     );
   }
@@ -38,7 +85,9 @@ class _CardGameViewState extends State<CardGameView> {
 
 class CardItem extends StatefulWidget {
   final String type;
+  late int index;
   bool isOpen = false;
+  bool isMatch = false;
 
   CardItem({super.key, required this.type});
 
@@ -55,6 +104,12 @@ class _CardItemState extends State<CardItem> {
         onTap: () {
           setState(() {
             widget.isOpen = !widget.isOpen;
+            print(
+                'index: ${widget.index} type: ${widget.type} open: ${widget.isOpen} match: ${widget.isMatch}');
+            sleep(const Duration(seconds: 2));
+            SingletonCardList().checkMatch(widget.index);
+            print(
+                'index: ${widget.index} type: ${widget.type} open: ${widget.isOpen} match: ${widget.isMatch}');
           });
         },
         child: Stack(
@@ -69,7 +124,7 @@ class _CardItemState extends State<CardItem> {
               )),
             ),
             Visibility(
-              visible: !widget.isOpen,
+              visible: !widget.isOpen || widget.isMatch,
               child: Container(
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
