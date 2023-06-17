5e6515bc023497b0450d9f15de429e7232bf7f61
haiph-dev <pinkocean.a16@gmail.com>
2023-06-17 17:44:14 +0700

    update

---
+++ lib/cardGame/cardItem.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CardItem {
+  final String type;
+  final String image;
+  bool isFlip = false;
+  // bool isMatch = false;
+
+  CardItem(this.type, this.image);
+
+  static List<CardItem> getLevel1() {
+    final cards = <CardItem>[];
+    cards.add(CardItem('dragon', 'lib/cardGame/images/chameleon.png'));
+    cards.add(CardItem('dragon', 'lib/cardGame/images/charizard.png'));
+    cards.add(CardItem('saur', 'lib/cardGame/images/bulbasaur.png'));
+    cards.add(CardItem('saur', 'lib/cardGame/images/venusaur.png'));
+    cards.add(CardItem('turle', 'lib/cardGame/images/blastoise.png'));
+    cards.add(CardItem('turle', 'lib/cardGame/images/squirtle.png'));
+
+    return cards;
+  }
+}
+
+class FrontCard extends StatelessWidget {
+  final CardItem card;
+  const FrontCard({super.key, required this.card});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          color: const Color.fromARGB(255, 153, 198, 235),
+          child: Image.asset(
+            card.image,
+          ),
+        ));
+  }
+}
+
+class BackCard extends StatefulWidget {
+  const BackCard({super.key});
+
+  @override
+  State<BackCard> createState() => _BackCardState();
+}
+
+class _BackCardState extends State<BackCard> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.asset('lib/cardGame/images/ball.jpg'),
+    );
+  }
+}
--- lib/cardGame/cardPage.dart
+++ lib/cardGame/cardPage.dart
@@ -1,30 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_techmaster/cardGame/item.dart';
-
-class Item {
-  final String type;
-  String? photo;
-  bool isFlip = false;
-  // bool isMatch = false;
-
-  Item(this.type);
-
-  static List<Item> getLevel1() {
-    final items = <Item>[];
-
-    items.add(Item('cat'));
-    items.add(Item('cat'));
-    items.add(Item('dog'));
-    items.add(Item('dog'));
-    items.add(Item('pig'));
-    items.add(Item('pig'));
-
-    items.shuffle();
-    return items;
-  }
-}
+import 'cardItem.dart';
 
 class CardGame extends StatelessWidget {
   const CardGame({super.key});
@@ -33,15 +8,13 @@ class CardGame extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Card Game'),
+          title: const Text('Card Game'),
         ),
         body: CardGameView());
   }
 }
 
 class CardGameView extends StatefulWidget {
-  List<Item> cardList = Item.getLevel1();
-
   CardGameView({super.key});
 
   @override
@@ -49,36 +22,58 @@ class CardGameView extends StatefulWidget {
 }
 
 class _CardGameViewState extends State<CardGameView> {
-  int lastFlip = -1;
+  List<CardItem> cards = CardItem.getLevel1();
   bool flipable = true;
-
-  void updateCardFlip(int index) async {
-    if (flipable == false) return;
-    widget.cardList[index].isFlip = true;
-    print('index: $index : ${widget.cardList[index].type}');
-    setState(() {});
-
-    if (lastFlip > -1) {
-      if (widget.cardList[index].type != widget.cardList[lastFlip].type) {
-        widget.cardList[index].isFlip = false;
-        widget.cardList[lastFlip].isFlip = false;
-        flipable = false;
-        await Future.delayed(Duration(seconds: 2), () {
-          setState(() {});
-        });
-
-        flipable = true;
-      }
-      lastFlip = -1;
-      print('lastFlip: $lastFlip');
-    } else {
-      lastFlip = index;
-      print('lastFlip: $lastFlip');
-    }
-  }
+  String lastFlip = '';
 
   @override
   Widget build(BuildContext context) {
-    return Container(padding: const EdgeInsets.all(10), child: FrontCard());
+    return GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+        itemCount: cards.length,
+        itemBuilder: (BuildContext context, int index) {
+          final card = cards[index];
+          if (card.isFlip) {
+            return FrontCard(card: card);
+          } else {
+            return GestureDetector(
+                onTap: () => onTapCallback(card), child: const BackCard());
+          }
+        },
+        physics: const NeverScrollableScrollPhysics());
+  }
+
+  void onTapCallback(CardItem justFlip) {
+    //not flipable cause still in wrong flip delay
+    if (!flipable) {
+      print('flipable: $flipable');
+      return;
+    }
+
+    //count how many card still not flip and same type with last flip
+
+    int countExistCard =
+        cards.where((e) => e.isFlip == false && e.type == lastFlip).length;
+    print('countExistCard: $countExistCard');
+
+    if (justFlip.type == lastFlip || lastFlip == '') {
+      //just flip correct or flip first of type
+      justFlip.isFlip = true;
+      lastFlip = justFlip.type;
+      setState(() {});
+    } else {
+      //just flip incorrect, delay 1s and flip all same type
+      flipable = false;
+      lastFlip = '';
+      Future.delayed(const Duration(seconds: 1), () {
+        for (CardItem card in cards) {
+          card.isFlip = false;
+        }
+        flipable = true;
+        setState(() {});
+      });
+    }
   }
 }
